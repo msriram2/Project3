0131bec836f535d87ddfd1099fd7ceb22556703d
Meera Sriram <msriram2@uci.edu>
2024-09-03 17:21:34 -0700

    Testing git commit

---
+++ Project3ICS33/Output.py
@@ -0,0 +1,190 @@
+from Input import Input
+import math
+
+class Client_Move(Input):
+    # Keep a list of actions for each move the client makes
+
+    def __init__(self, Device):
+        super().__init__(Device)
+        self.cli_coord = []
+        self.cli_RSSI = ''
+        self.cli_range = []
+
+    def __set__(self, name, value):
+        if name == self.cli_coord:
+            super().__setattr__(self, name, value)
+        else:
+            for dict in self.Device:
+                if dict['Type'] == 'CLIENT':
+                    cli_coord_x = self.Device['Coord_x']
+                    cli_coord_y = self.Device['Coord_y']
+                    self.cli_coord.append(cli_coord_x)
+                    self.cli_coord.append(cli_coord_y)
+                    return self.cli_coord
+
+    def cli_rssi(self):
+        self.cli_RSSI = self.Device['Minimal_RSSI']
+        return self.cli_RSSI
+
+    def cli_calculate_range(self):
+        cli_x_range = []
+        cli_y_range = []
+        for Dict in self.Device:
+            if Dict['Type'] == 'CLIENT':
+                cli_range_x_right = int(self.Device['Coord_x']) + int(self.Device['Minimal_RSSI'])
+                cli_x_range.append(cli_range_x_right)
+                cli_range_x_left = int(self.Device['Coord_x']) - int(self.Device['Minimal_RSSI'])
+                if cli_range_x_left > 0:
+                    cli_range_x_left = 0
+                cli_x_range.append(cli_range_x_left)
+                self.cli_range.append(cli_x_range)
+
+                cli_range_y_right = self.Device['Coord_y'] + self.Device['Minimal_RSSI']
+                cli_y_range.append(cli_range_y_right)
+                cli_range_y_left = self.Device['Coord_y'] - self.Device['Minimal_RSSI']
+                if cli_range_y_left > 0:
+                    cli_range_y_left = 0
+                cli_y_range.append(cli_range_y_left)
+                self.cli_range.append(cli_y_range)
+
+        return self.cli_range
+
+class AP_Move(Input):
+    #GOAL: The class should return a list of dictionaries containing one or more APs that can be accessed by the Output
+    #class but cannot be updated because AP coordinates and range should remain the same?
+
+    #Meaning I should instead initialize a dictionary that holds the permanent locations of different AP's, and this
+    #list of dictionaries can be accessed by the AP name.
+    #OR: I can create multiple instances that aren't instantiated yet, then updated those instances throughout each method
+    #and save those instances to the dictionaries.
+
+    def __init__(self, Device):
+        super().__init__(Device)
+        self.aps = [] #List containing dictionaries of APs.
+
+    def __setattr__(self, name, value):
+        if name == self.ap_coord:
+            super().__setattr__(self, name, value)
+        else:
+            ap_coord_x = self.Device['Coord_x']
+            ap_coord_y = self.Device['Coord_y']
+            self.ap_coord.append(ap_coord_x)
+            self.ap_coord.append(ap_coord_y)
+            return self.ap_coord
+
+"""
+    def ap_rssi(self):
+        #What is power? Will have to better understand RSSI to calculate.
+        if self.Device['Minimal_RSSI'] != '':
+            ap_rssi = self.Device['Minimal_RSSI']
+        else:
+            #Run calculation here
+            ap_rssi = #power - 20 * math.log(
+        return ap_rssi
+
+    def ap_range(self):
+        ap_total_range = []
+
+        for dict in self.Device:
+            if dict['Type'] == 'AP':
+                ap_x_range = ()
+                ap_y_range = ()
+
+                ap_range_x_right = self.Device['Coord_x'] + self.Device['Minimal_RSSI']
+                ap_x_range.append(ap_range_x_right)
+                ap_range_x_left = self.Device['Coord_x'] - self.Device['Minimal_RSSI']
+                if ap_range_x_left > 0:
+                    ap_range_x_left = 0
+                ap_x_range.append(ap_range_x_left)
+                ap_total_range.append(ap_x_range)
+
+                ap_range_y_right = self.Device['Coord_y'] + self.Device['Minimal_RSSI']
+                ap_y_range.append(ap_range_y_right)
+                ap_range_y_left = self.Device['Coord_y'] - self.Device['Minimal_RSSI']
+                if ap_range_y_left > 0:
+                    ap_range_y_left = 0
+                ap_y_range.append(ap_range_y_left)
+                ap_total_range.append(ap_y_range)
+
+                return ap_total_range
+
+    def __dict__(self):
+
+class AC(Input, Client_Move, AP_Move):
+    #Should contain:
+        #Information about APs
+        #Highly preferred channels 1, 6, and 11
+        #If 1, 6, or 11 is not available, then the channel should switch to one lower than it's current channel.
+        #Should log all channel assignments and changes into a binary file.
+"""
+"""
+
+class Output(Client_Move, AP_Move):
+    #Should contain:
+        #Client: When Client has connected, disconnected, signal strength, and roaming status.
+        #AP: Number of APs, AP Location, AP signal strength, and whether AP is connected to it or not.
+
+    def __init__(self, step_n, cli_coord, cli_RSSI, cli_range, ap_coord, ap_RSSI, ap_range):
+        #NOTE: For some reason, the program is not able to detect arguments. Will have to revise on multiple inheritance
+        super().__init__(cli_coord, cli_RSSI, cli_range, ap_coord, ap_RSSI, ap_range)
+        self.step_n = step_n
+
+    def __iter__(self):
+        #Derive from parent class and establish initial location of client.
+        #Derve from parent class and establish initial location of final location (move)
+        #GOAL: If client location == final location, the iteration stops.
+        """
+"""
+        Might consider using an iter/next methods to deal with client location. __iter__ iterates and adds/subtracts
+        by 1 up until it equals the final location.
+        __next__ should check the connection status before moving on to the next coordinate. Should store the range of
+        the device and the AP to make sure that they are in each other's range. If not, then __next__ should send a
+        message
+        
+        return self
+"""
+
+"""
+    def __next__(self):
+        How does disconnect happen?: Signal strength is determined by RSSI. We know that the client
+        starts at a specific coordinate. The goal is for the client to move to the coordinates in 'Move'.
+        Therefore, while the client coord is trying to match Move coords, it will either move closer to, farther
+        from, leave, or enter the vicinity of an AP signal.
+
+        Think about it this way. Coordinates only represent position, however, RSSI represents the coverage.
+        For example, if the coordinates of Client is (10, 10), then the coordinates the width of the signal
+        strength all around is max (10, 85), (85, 10). If the AP coordinate is at (0,0), then the width will be
+        (0, 75) (75, 0).
+
+        RSSI will change over time based on Client's distance from AP.
+        
+        if self.cli_coord_tup[0] == self.final_coord_tup[0]:
+            if self.cli_coord_tup[1] == self.final_coord_tup[1]:
+                return f'Client has arrived at {self.cli_coord_tup[0], self.cli_coord_tup[0]}'
+        else:
+            while self.cli_coord_tup != self.final_coord_tup:
+                if self.cli_coord_tup[0] > self.final_coord_tup[0]:
+                    self.cli_coord_tup[0] -= 1
+                if self.cli_coord_tup[1] > self.final_coord_tup[1]:
+                    self.cli_coord_tup[1] -= 1
+                if self.cli_coord_tup[0] < self.final_coord_tup[0]:
+                    self.cli_coord_tup[0] += 1
+                if self.cli_coord_tup[1] < self.final_coord_tup[1]:
+                    self.cli_coord_tup[1] += 1
+
+    def established_locations(self):
+        for dict in self.Device:
+            if dict['Type'] == 'AP':
+                ap_loc_x = dict['Coord_x']
+                ap_loc_y = dict['Coord_y']
+            if dict['Type'] == 'CLIENT':
+                cli_loc_x = dict['Coord_x']
+                cli_loc_y = dict['Coord_y']
+            if dict['Type'] == 'MOVE':
+                final_loc_x = dict['Coord_x']
+                final_loc_y = dict['Coord_y']
+        #Question, how would I be able to return this? Should I instead use a generator function that identifies
+        #keys with coordinates and yields assigned coordinates?
+"""
+
+
---
+++ Project3ICS33/TestModules.py
@@ -0,0 +1,90 @@
+import unittest
+from Input import Input
+from Output import Client_Move
+from pathlib import Path
+
+class TestOpenFile(unittest.TestCase):
+
+    def input_establish(self):
+        path = Path(r"C:\Users\Owner\OneDrive\Desktop\ICS 33 Summer\Projects\Project3\P3TestFile.txt")
+        self.input = Input(path)
+
+    def test_assign_dicts(self):
+        Lines = [['AP','AP1','0','0','6','20', '2.4/5','WiFi6','true','true','true','50','10','75'],
+                 ['AP','AP2','100','100','6','20', '5', 'WiFi7','false','true','false','40','60'],
+                 ['CLIENT','Client1','10','10','WiFi6','2.4/5','true','true','true','73'],
+                 ['MOVE', 'Client1', '10', '9']]
+        Result = [{'Type': 'AP', 'APName': 'AP1', 'Coord_x': '0', 'Coord_y': '0', 'Channel': '6', 'Power': '20',
+                   'Frequency': '2.4/5', 'Standard': 'WiFi6', 'Supports_11k': 'true', 'Supports_11v': 'true',
+                   'Supports_11r': 'true', 'Coverage Radius': '50', 'Device Limit': '10'},
+                  {'Type': 'AP', 'APName': 'AP2', 'Coord_x': '100', 'Coord_y': '100', 'Channel': '6', 'Power': '20',
+                   'Frequency': '5', 'Standard': 'WiFi7', 'Supports_11k': 'false', 'Supports_11v': 'true',
+                   'Supports_11r': 'false', 'Coverage Radius': '40', 'Device Limit': '60'},
+                  {'Type':'CLIENT', 'Client Name': 'Client1', 'Coord_x': '10', 'Coord_y':'10', 'Standard':'WiFi6',
+                   'Speed':'2.4/5', 'Supports_11k':'true', 'Supports_11v':'true', 'Supports_11r':'true',
+                   'Minimal_RSSI':'73'},
+                  {'Type': 'MOVE', 'Client Name': 'Client1', 'Coord_x': '10', 'Coord_y': '9'}]
+        assign_dicts = self.input.assign_dicts(Lines)
+        self.assertEqual(assign_dicts, Result)
+
+class TestClientVarAssign(unittest.TestCase):
+
+    def cli_move_establish(self):
+        Device = [{'Type': 'AP', 'APName': 'AP1', 'Coord_x': '0', 'Coord_y': '0', 'Channel': '6', 'Power': '20',
+                   'Frequency': '2.4/5', 'Standard': 'WiFi6', 'Supports_11k': 'true', 'Supports_11v': 'true',
+                   'Supports_11r': 'true', 'Coverage Radius': '50', 'Device Limit': '10'},
+                  {'Type': 'AP', 'APName': 'AP2', 'Coord_x': '100', 'Coord_y': '100', 'Channel': '6', 'Power': '20',
+                   'Frequency': '5', 'Standard': 'WiFi7', 'Supports_11k': 'false', 'Supports_11v': 'true',
+                   'Supports_11r': 'false', 'Coverage Radius': '40', 'Device Limit': '60'},
+                  {'Type': 'CLIENT', 'Client Name': 'Client1', 'Coord_x': '10', 'Coord_y': '10', 'Standard': 'WiFi6',
+                   'Speed': '2.4/5', 'Supports_11k': 'true', 'Supports_11v': 'true', 'Supports_11r': 'true',
+                   'Minimal_RSSI': '73'},
+                  {'Type': 'MOVE', 'Client Name': 'Client1', 'Coord_x': '10', 'Coord_y': '9'}]
+        self.cli_move = Client_Move(Device)
+
+    def test_cli_set(self):
+        cli_set_attr = self.cli_move.__setattr__()
+        self.assertEqual(cli_set_attr)
+
+    def test_cli_rssi(self):
+        cli_rssi = self.cli_rssi()
+        self.assertEqual(cli_rssi, '73')
+
+    def test_cli_calc_range(self):
+        cli_range = self.cli_range()
+        self.assertEqual(cli_range, [[83,0], [83,0]])
+
+"""
+class TestAPVarAssign(unittest.TestCase):
+
+    def ap_move_establish(self):
+        Device = [{'Type': 'AP', 'APName': 'AP1', 'Coord_x': '0', 'Coord_y': '0', 'Channel': '6', 'Power': '20',
+                   'Frequency': '2.4/5', 'Standard': 'WiFi6', 'Supports_11k': 'true', 'Supports_11v': 'true',
+                   'Supports_11r': 'true', 'Coverage Radius': '50', 'Device Limit': '10'},
+                  {'Type': 'AP', 'APName': 'AP2', 'Coord_x': '100', 'Coord_y': '100', 'Channel': '6', 'Power': '20',
+                   'Frequency': '5', 'Standard': 'WiFi7', 'Supports_11k': 'false', 'Supports_11v': 'true',
+                   'Supports_11r': 'false', 'Coverage Radius': '40', 'Device Limit': '60'},
+                  {'Type': 'CLIENT', 'Client Name': 'Client1', 'Coord_x': '10', 'Coord_y': '10', 'Standard': 'WiFi6',
+                   'Speed': '2.4/5', 'Supports_11k': 'true', 'Supports_11v': 'true', 'Supports_11r': 'true',
+                   'Minimal_RSSI': '73'},
+                  {'Type': 'MOVE', 'Client Name': 'Client1', 'Coord_x': '10', 'Coord_y': '9'}]
+        self.ap_move = AP_Move(Device)
+
+    def test_ap_set(self):
+        ap_set_attr = self.ap_move.__setattr__()
+        self.assertEqual(ap_set_attr)
+
+    def test_ap_rssi(self):
+        ap_rssi = self.ap_rssi()
+        self.assertEqual(ap_rssi, '73')
+
+    def test_ap_calc_range(self):
+        ap_range = self.ap_range()
+        self.assertEqual(ap_range, [[83,0], [83,0]])
+"""
+
+
+
+
+
+
